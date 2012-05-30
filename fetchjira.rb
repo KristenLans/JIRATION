34f226d6d196339de616bd106cc862cec4a40ac5
Kristen Lans <klans@eol.org>
2012-05-30 11:51:05 -0400

    Fixed instance variable for template.

--- fetchjira.rb
+++ fetchjira.rb
@@ -5,14 +5,15 @@ require 'haml'
 # we need a web server. We're using the sinatra gem girl!
 require 'sinatra'
 
+require './lib/jira.rb'
 
 # when a user goes to the root of the app, jiralicious.eol.org, they will see a list of JIRA issues.
 get '/' do
   # We need to sort issues by userpain
   issues = fetch_issues
-  issues = issues.sort_by { |a| [ a.matuserpain ] }
-  
+  @issues = issues.sort_by { |a| [ a.matuserpain ] }
+  puts issues.class
   # render results to browser
-  haml :index, :locals => {:issues => issues}
+  haml :index, :locals => {:issues => @issues}
 end
 
